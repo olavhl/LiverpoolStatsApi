5e4f1488d778c4e596f6e559e53f4916b337e5ab
Olav Hartwedt Larsen <ohalar@gmail.com>
2021-11-01 13:51:05 +0100

    Removed WeatherForecast

--- Services/PlayerService.cs
+++ Services/PlayerService.cs
@@ -7,20 +7,20 @@ namespace LiverpoolStatsApi.Services
 {
     public class PlayerService
     {
-    private readonly IMongoCollection<Player> _players;
+        private readonly IMongoCollection<Player> _players;
 
-    public PlayerService(ILiverpoolStatsDatabaseSettings settings)
-    {
-        var client = new MongoClient( settings.ConnectionString );
-            var database = client.GetDatabase( settings.DatabaseName );
-            _players = database.GetCollection<Player>( settings.PlayersCollectionName );
-    }
+        public PlayerService(ILiverpoolStatsDatabaseSettings settings)
+        {
+            var client = new MongoClient( settings.ConnectionString );
+                var database = client.GetDatabase( settings.DatabaseName );
+                _players = database.GetCollection<Player>( settings.PlayersCollectionName );
+        }
 
-    public List<Player> Get()
-    {
-        return _players.Find( player => true ).ToList();
-    }
-        
+        public List<Player> Get()
+        {
+            return _players.Find( player => true ).ToList();
+        }
+            
     }
 
 }
