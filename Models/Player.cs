15f0e580a6eb744e61270eceb06e3c53e3024d49
Olav Hartwedt Larsen <ohalar@gmail.com>
2021-11-09 10:57:09 +0100

    Added more fields to the database in MongoDb

--- Models/Player.cs
+++ Models/Player.cs
@@ -10,5 +10,8 @@ namespace LiverpoolStatsApi.Models
         public string Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public string Position { get; set; }
     }
 }
